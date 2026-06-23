7e46bdd7e41f3edaddf875191583f0af6a324a27
ssterplam <kata.diaza@duocuc.cl>
2026-06-23 19:37:18 -0400

    Se agregó truncate table para que los datos se reinicien con el microservicio.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,7 @@
+-- Resetea la tabla y el contador de IDs a 1
+TRUNCATE TABLE estudios;
+
+-- Se insertan los datos
 INSERT INTO estudios (nombre, pais_origen, ano_fundacion, sitio_web) VALUES
     ('FromSoftware', 'Japón', 1986, 'https://www.fromsoftware.jp'),
     ('Naughty Dog', 'Estados Unidos', 1984, 'https://www.naughtydog.com'),
