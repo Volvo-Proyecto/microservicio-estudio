3ca38e843bbcc3e1ab816046707e81fe251580e4
ssterplam <kata.diaza@duocuc.cl>
2026-06-23 19:14:40 -0400

    Se agrega el archivo data.sql con los estudios de videojuegos.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO estudios (nombre, pais_origen, ano_fundacion, sitio_web) VALUES
+    ('FromSoftware', 'Japón', 1986, 'https://www.fromsoftware.jp'),
+    ('Naughty Dog', 'Estados Unidos', 1984, 'https://www.naughtydog.com'),
+    ('Team Cherry', 'Australia', 2014, 'https://www.teamcherry.com.au'),
+    ('Firaxis Games', 'Estados Unidos', 1996, 'https://firaxis.com'),
+    ('Capcom', 'Japón', 1983, 'https://www.capcom.com');
