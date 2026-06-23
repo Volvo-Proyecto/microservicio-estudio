105271cce4fd8a5bec97be55161f095545874478
ssterplam <kata.diaza@duocuc.cl>
2026-06-23 12:03:44 -0400

    Creación de microservicio estudio

---
+++ src/main/java/cl/volvo/estudio/EstudioApplication.java
@@ -0,0 +1,13 @@
+package cl.volvo.estudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/volvo/estudio/EstudioApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.volvo.estudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
